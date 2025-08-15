f26593c68274867db7aedbbc3e6b816175e7717a
Rodrigo Pretes <rodrigopretes64@gmail.com>
2025-08-15 11:16:57 -0300

    feat: passando id de forma forçada para a seed casos

--- db/seeds/casos.js
+++ db/seeds/casos.js
@@ -4,19 +4,18 @@
  */
 exports.seed = async function(knex) {
   await knex('casos').del();
-  const agentes = await knex("agentes").select("id", "nome");
   await knex('casos').insert([
     {
       titulo: "Roubo ao mercado",
       descricao: "Ocorrido no bairro Centro às 21h.",
       status: "aberto",
-      agente_id: agentes[0].id
+      agente_id: 1
     },
     {
       titulo: "Fraude bancária",
       descricao: "Transações suspeitas em conta PJ.",
       status: "solucionado",
-      agente_id: agentes[1].id
+      agente_id: 2
     }
   ]);
 };
